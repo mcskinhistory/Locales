d62c9cc528688ced063356d22342d4efec8bcc86
Zeryther <zeryther@gmail.com>
2018-07-22 03:17:03 +0200

    Keep Locale classes in Locale repository

---
+++ Locale.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Gigadrive\i18n;
+
+/**
+ * Represents a Locale translated via weblate
+ * 
+ * @package i18n
+ * @author Gigadrive (support@gigadrivegroup.com)
+ * @copyright 2016-2018 Gigadrive
+ * @link https://gigadrivegroup.com/dev/technologies
+ */
+class Locale {
+	/**
+	 * @access private
+	 * @var string $code
+	 */
+	private $code;
+
+	/**
+	 * @access private
+	 * @var string $name
+	 */
+	private $name;
+
+	/**
+	 * @access private
+	 * @var string $localizedName
+	 */
+	private $localizedName;
+
+	/**
+	 * @access private
+	 * @var array $phrases
+	 */
+	private $phrases;
+
+	/**
+	 * Constructor
+	 * 
+	 * @access public
+	 * @param string $code Language code from weblate
+	 */
+	public function __construct($code){
+		$this->code = $code;
+	}
+
+	/**
+	 * Gets the language code
+	 * 
+	 * @access public
+	 * @return string
+	 */
+	public function getCode(){
+		return $this->code;
+	}
+
+	/**
+	 * Gets the locale name
+	 * 
+	 * @access public
+	 * @return string
+	 */
+	public function getName(){
+		if(is_null($this->name))
+			$this->reload();
+
+		return $this->name;
+	}
+
+	/**
+	 * Gets the localized locale name (returns null for English and similar)
+	 * 
+	 * @access public
+	 * @return string
+	 */
+	public function getLocalizedName(){
+		return $this->localizedName;
+	}
+
+	/**
+	 * Gets the phrase array
+	 * 
+	 * @access public
+	 * @return array
+	 */
+	public function getPhrases(){
+		if(is_null($this->phrases))
+			$this->reload();
+
+		return $this->phrases;
+	}
+
+	/**
+	 * Reloads phrases and locale data from the file system
+	 * 
+	 * @access public
+	 */
+	public function reload(){
+		$folder = $_SERVER["DOCUMENT_ROOT"] . "/library/i18n/Locales/" . i18n::$componentName . "/" . $this->code . "/";
+
+		if(file_exists($folder) && is_dir($folder)){
+			if(file_exists($folder . "translation.json") && file_exists($folder . "settings.json")){
+				$settings = json_decode(file_get_contents($folder . "settings.json"),true);
+
+				if(isset($settings["name"]))
+					$this->name = $settings["name"];
+
+				if(isset($settings["localizedName"]))
+					$this->localizedName = $settings["localizedName"];
+
+				$translation = json_decode(file_get_contents($folder . "translation.json"),true);
+
+				$this->phrases = [];
+				foreach($translation as $key => $value){
+					$this->phrases[$key] = $value;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Gets whether the locale was properly loaded and validated
+	 * 
+	 * @access public
+	 * @return bool
+	 */
+	public function isValid(){
+		return !is_null($this->name);
+	}
+
+	/**
+	 * Returns a translated phrase by ID, replacing variables
+	 * 
+	 * @access public
+	 * @param string $phrase
+	 * @param array $variables
+	 * @return string
+	 */
+	public function getTranslatedMessage($phrase, $variables = null){
+		$phrase = trim(strtolower($phrase));
+
+		if(array_key_exists($phrase,$this->phrases)){
+			$r = $this->phrases[$phrase];
+
+			if(!is_null($variables) && is_array($variables) && count($variables) > 0){
+				for ($i = 0; $i < count($variables); $i++) { 
+					$var = $variables[$i];
+
+					$r = str_replace("{" . $i . "}",$var,$r);
+				}
+			}
+
+			return $r;
+		} else {
+			return $phrase;
+		}
+	}
+}
---
+++ i18n.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Gigadrive\i18n;
+
+/**
+ * Internationalization utilities
+ * 
+ * @package i18n
+ * @author Gigadrive (support@gigadrivegroup.com)
+ * @copyright 2016-2018 Gigadrive
+ * @link https://gigadrivegroup.com/dev/technologies
+ */
+class i18n {
+	/**
+	 * @access public
+	 * @var string $componentName The locale component name (subfolder)
+	 */
+	public static $componentName;
+
+	/**
+	 * Returns a translated phrase by ID, replacing variables, uses the locale found in i18n::getCurrentLanguage()
+	 * 
+	 * @access public
+	 * @param string $phrase
+	 * @param array $variables
+	 * @return string
+	 */
+	public static function getTranslatedMessage($phrase, $variables = null){
+		$l = self::getCurrentLanguage();
+
+		if(!is_null($l)){
+			return $l->getTranslatedMessage($phrase,$variables);
+		} else {
+			return trim(strtolower($phrase));
+		}
+	}
+
+	/**
+	 * Gets the current user's locale
+	 * 
+	 * @access public
+	 * @return Locale
+	 */
+	public static function getCurrentLanguage(){
+		$code = null;
+		if(isset($_COOKIE["lang"])){
+			$code = $_COOKIE["lang"];
+		}
+
+		$locale = self::getLocale($code);
+		if(!is_null($locale))
+			return $locale;
+
+		// TODO: Check for settings associated with the user's account and the browser language
+		return self::getLocale("en");
+	}
+
+	/**
+	 * Returns a locale defined by the code passed, if the locale can't be found it will default to English, if English can't be found it will return null
+	 * 
+	 * @access public
+	 * @param string $code
+	 * @return string
+	 */
+	public static function getLocale($code){
+		$code = trim(strtolower($code));
+		$i = self::Instance();
+
+		if(array_key_exists($code,$i->getLocales())){
+			return $i->getLocales()[$code];
+		} else {
+			if($code == "en"){
+				return null;
+			} else {
+				return self::getLocale($code);
+			}
+		}
+	}
+
+	/**
+	 * @access private
+	 * @var array $locales
+	 */
+	private $locales;
+
+	/**
+	 * Gets the Internationalization class instance
+	 * 
+	 * @access public
+	 * @return i18n
+	 */
+	public static function Instance(){
+		static $inst = null;
+		if($inst == null){
+			$inst = new self(LOCALE_COMPONENT);
+		}
+
+		return $inst;
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @access protected
+	 */
+	protected function __construct(){
+		$this->loadLocales();	
+	}
+
+	/**
+	 * Reloads all locales from the file system
+	 * 
+	 * @access public
+	 */
+	public function loadLocales(){
+		$n = "i18n_locales";
+
+		if(\CacheHandler::existsInCache($n)){
+			$this->locales = \CacheHandler::getFromCache($n);
+		} else {
+			$folder = $_SERVER["DOCUMENT_ROOT"] . "/library/i18n/Locales/" . self::$componentName . "/";
+
+			if(file_exists($folder) && is_dir($folder)){
+				// TODO
+			}
+
+			// TODO: Save to cache
+		}
+	}
+
+	/**
+	 * Gets all loaded locales
+	 * 
+	 * @access public
+	 * @return array
+	 */
+	public function getLocales(){
+		return $this->locales;
+	}
+}
